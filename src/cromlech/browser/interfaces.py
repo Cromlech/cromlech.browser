67053464a25f5c74193ad7870410b8d14dfd4726
nitro <nitro@dr-code.org>
2011-03-30 01:47:55 +0200

    - moved interfaces from dolmen.view to cromlech.browser

--- src/cromlech/browser/interfaces.py
+++ src/cromlech/browser/interfaces.py
@@ -21,9 +21,48 @@ class IRenderer(Interface):
         """returns a response object with the body and headers set.
         """
 
+
+class IBaseClasses(Interface):
+    View = Attribute("Base class for browser views.")
+
+
+class IDirectives(Interface):
+
+    def request(interface):
+        """Define on which request a component is registered.
+        This can be used to narrow to a skin type.
+        """
+
+    def view(view):
+        """Define on which view a viewlet manager/viewlet is registered.
+        """
+
+
 class IView(Interface):
-    pass
+    """Grok views all provide this interface."""
+
+    context = Attribute('context', "Object that the view presents.")
+
+    request = Attribute('request', "Request that the view was looked up with.")
+
+    def __call__():
+        """Returns a full Response object.
+        """
+
+    def update():
+        """
+        """
+
+    def render():
+        """
+        """
+
+
+class ISecuredItem(Interface):
+    """
+    """
 
 
 class IViewlet(Interface):
-    pass
+    """
+    """
