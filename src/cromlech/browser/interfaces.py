e317925935e2b58bea93452332f9ecb28cfc18af
Souheil CHELFOUH <trollfot@gmail.com>
2011-04-04 14:46:20 +0200

    Reviewed the interfaces and removed the Component/Collection, for now

--- src/cromlech/browser/interfaces.py
+++ src/cromlech/browser/interfaces.py
@@ -9,22 +9,27 @@ class IRenderer(Interface):
     Most of the time, it gets an object and a request and returns
     a response but may also return raw data.
     """
+    def namespace():
+        """A dict representing the values accessible by the templates.
+        """
 
     def update(*args, **kwargs):
-        """Prepares the rendering
+        """Prepares the rendering.
         """
 
     def render():
-        """Returns the raw data
+        """Returns the raw data.
         """
 
-    def __call__():
-        """
-        """
+
+class ILayout(IRenderer):
+    """Global layout view.
+    """
 
 
 class IView(IRenderer):
-    """Grok views all provide this interface"""
+    """Grok views all provide this interface.
+    """
 
     context = Attribute('context', "Object that the view presents.")
     request = Attribute('request', "Request that the view was looked up with.")
@@ -32,103 +37,3 @@ class IView(IRenderer):
     def __call__():
         """Returns a response object with the body and headers set.
         """
-
-
-class ISecuredItem(Interface):
-    """
-    """
-
-
-class ILayout(IRenderer):
-    """Global layout view
-    """
-
-
-class IComponent(Interface):
-    """A named component
-    
-    This is intended to be a generic mechanism for composition mechanism
-    """
-    identifier = Attribute(u"Component id")
-    title = Attribute(u"Component title")
-
-    def clone(new_identifier=None):
-        """Return a clone of the new component, with identifier
-        new_identifier if it is not None.
-        """
-
-
-class ICollection(Interface):
-    """Support to manage a collection of ordered named components.
-    """
-    
-    type = Attribute(
-        u"Interface restricting the type of component")
-
-    def append(component):
-        """Add a new component to the collection. Modify the current
-        collection.
-        """
-
-    def extend(*component):
-        """Create/Add a list of components to the collection. Modify
-        the current collection.
-        """
-
-    def get(id, default=None):
-        """Return the component with the given ID."""
-
-    def select(*ids):
-        """Return a copy containing only the given named components."""
-
-    def omit(*ids):
-        """Return a copy containing all but none of the given named
-        components.
-        """
-
-    def copy():
-        """Return a copy of the collection."""
-
-    def clear():
-        """Empty the collection: remove all components from it."""
-
-    def keys():
-        """Return all components id contained in the collection."""
-
-    def __add__(other):
-        """Create a collection as copy of self, and add value for
-        other component or collection.
-        """
-
-    def __getitem__(id):
-        """Return the given component identified by id or raise
-        KeyError.
-        """
-
-    def __contains__(id):
-        """Return true if the collection contains a component
-        identified by id.
-        """
-
-    def __iter__():
-        """Return an iterator on the components.
-        """
-
-    def __len__():
-        """Return the numbre of components.
-        """
-
-
-class IManager(Interface):
-    components = Attribute("")
-
-
-class IViewlet(IRenderer):
-    """a viewlet is a component rendering a small part of the global view
-    driven by a manager
-    """
-
-
-class IViewletManager(IManager, IRenderer):
-    """compose a set of viewlet together and render in a more global view
-    """
