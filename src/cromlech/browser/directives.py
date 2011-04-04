304c32cc6571099ce096f296bb17be22ab5243d3
Alex Garel <alex.garel@gmail.com>
2011-04-04 17:16:21 +0200

    manager becomes slot and added docstring

--- src/cromlech/browser/directives.py
+++ src/cromlech/browser/directives.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+"""
+Some directives to link components
+"""
 
 import martian
 from zope.interface import Interface
@@ -6,16 +9,20 @@ from cromlech.browser.interfaces import IManager
 
 
 def default_view_name(factory, module=None, **data):
+    """A view is, by default registered under its lowercase name."""
     return factory.__name__.lower()
 
 
 class view(martian.Directive):
+    """specify which kind of view an component
+    (eg. viewlet or slot) applies to"""
     scope = martian.CLASS_OR_MODULE
     store = martian.ONCE
     default = Interface
 
 
-class manager(martian.Directive):
+class slot(martian.Directive):
+    """specify which kind of slot a component (eg. viewlet) is part of"""
     scope = martian.CLASS_OR_MODULE
     store = martian.ONCE
     default = IManager
